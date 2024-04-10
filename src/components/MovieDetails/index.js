e0326a5b08992b1a984d76089e6d60cc687d2ae0
ANJIYADAV-147 <anjiyadav.kaithagamail.com>
2024-04-10 11:45:13 +0530

    solution

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,101 +1,95 @@
-import React, { useEffect ,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../Header';
 import { useParams } from 'react-router-dom';
-import './index.css'
-
+import './index.css';
 
 const MovieDetails = () => {
-    const [movieDetailList,setMovieDetailList]=useState([]);
-    const [castDetailsList,setCastDetailsList]=useState([]);
+  const [movieDetailList, setMovieDetailList] = useState([]);
+  const [castDetailsList, setCastDetailsList] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
-    getMoviesDetails();
-    getCastDetails();
-  }, []);
+    const getMoviesDetails = async () => {
+      const url = `https://api.themoviedb.org/3/movie/${id}?api_key=fa030b3d520335437ba6c4cbb262cfcb&language=en-US`;
+      const options = {
+        method: 'GET',
+      };
 
-  const getMoviesDetails = async () => {
-    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=fa030b3d520335437ba6c4cbb262cfcb&language=en-US`;
-    const options = {
-      method: 'GET',
+      try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        const updatedData = {
+          id: data.id,
+          title: data.title,
+          overview: data.overview,
+          posterPath: data.poster_path,
+          releaseDate: data.release_date,
+          genres: data.genres,
+          runtime: data.runtime,
+          voteAverage: data.vote_average,
+          backdropPath: data.backdrop_path,
+        };
+        setMovieDetailList(updatedData);
+      } catch (error) {
+        console.error('Error fetching movie details:', error);
+      }
     };
 
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-      const updatedData={
-        id:data.id,
-        title:data.title,
-        overview:data.overview,
-        posterPath:data.poster_path,
-        releaseDate:data.release_date,
-        genres:data.genres,
-        runtime:data.runtime,
-        voteAverage:data.vote_average,
-        backdropPath:data.backdrop_path,
+    const getCastDetails = async () => {
+      const url = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=fa030b3d520335437ba6c4cbb262cfcb&language=en-US`;
+      const options = {
+        method: 'GET',
+      };
+      try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        const updatedCastData = data.cast.map(eachDetails => ({
+          id: eachDetails.id,
+          profilePath: eachDetails.profile_path,
+          name: eachDetails.name,
+          character: eachDetails.character,
+        }));
+        setCastDetailsList(updatedCastData);
+      } catch (error) {
+        console.log('Error fetching cast details:', error);
       }
-      setMovieDetailList(updatedData);
-    } catch (error) {
-      console.error('Error fetching movie details:', error);
-    }
-  };
+    };
 
-  const getCastDetails=async()=>{
-    const url=`https://api.themoviedb.org/3/movie/${id}/credits?api_key=fa030b3d520335437ba6c4cbb262cfcb&language=en-US`
-    const options={
-        method:'GET',
-    }
-    try{
-        const response=await fetch(url,options)
-        const data=await response.json()
-        console.log(data)
-        const updatedCastData=data.cast.map(eachDetails=>({
-            id:eachDetails.id,
-            profilePath:eachDetails.profile_path,
-            name:eachDetails.name,
-            character:eachDetails.character,
-        }))
-        setCastDetailsList(updatedCastData)
-        console.log(updatedCastData)
-    }
-    catch(error){
-        console.log('Error fetching cast details:',error);
-    }
-
-  }
+    getMoviesDetails();
+    getCastDetails();
+  }, [id]); // Includes 'id' in the dependency array
 
   return (
-    <div className='movie-container'>
+    <div className="movie-container">
       <Header />
-      <div className='details-container'>
-      <div className='movies-details-list-container'>
-        <div className='img-title-container'>
-            <img src={`https://image.tmdb.org/t/p/w500${movieDetailList.posterPath}`} alt={movieDetailList.posterPath} className='posterImg'/>
-            <div className='title-container'>
-                <h1 className='title'>{movieDetailList.title}</h1>
-                <p className='rating'>vote_average: {movieDetailList.voteAverage}</p>
-                <p className='duration'>duration: {movieDetailList.runtime} min</p>
-                <p className='releaseDate'>Release Date: {movieDetailList.releaseDate}</p>
+      <div className="details-container">
+        <div className="movies-details-list-container">
+          <div className="img-title-container">
+            <img src={`https://image.tmdb.org/t/p/w500${movieDetailList.posterPath}`} alt={movieDetailList.posterPath} className="posterImg" />
+            <div className="title-container">
+              <h1 className="title">{movieDetailList.title}</h1>
+              <p className="rating">vote: {movieDetailList.voteAverage}</p>
+              <p className="duration">duration: {movieDetailList.runtime} min</p>
+              <p className="releaseDate">Release Date: {movieDetailList.releaseDate}</p>
             </div>
-
+          </div>
+          <div className="overview-container">
+            <h1 className="heading">Overview</h1>
+            <p className="overview">{movieDetailList.overview}</p>
+          </div>
         </div>
-        <div className='overview-container'> 
-        <h1 className='heading'>Overview</h1>
-        <p className='overview'>{movieDetailList.overview}</p>
-        </div>
+        <img src={`https://image.tmdb.org/t/p/w500${movieDetailList.backdropPath}`} alt={movieDetailList.backdropPath} className="backdrop_path" />
       </div>
-      <img src={`https://image.tmdb.org/t/p/w500${movieDetailList.backdropPath}`} alt={movieDetailList.backdropPath} className='backdrop_path'/>
-      </div>
-      <h1 className='heading'>Cast</h1>
-      <div className='cast-container'>
+      <h1 className="heading">Cast</h1>
+      <div className="cast-container">
         {castDetailsList.map(castMember => (
-            <div key={castMember.id} className="cast-member">
-                <img src={`https://image.tmdb.org/t/p/w500${castMember.profilePath}`} alt={castMember.name} className='profilePath'/>
-                <p className='name'>{castMember.name}</p>
-                <p className='character'>{castMember.character}</p>
-            </div>
+          <div key={castMember.id} className="cast-member">
+            <img src={`https://image.tmdb.org/t/p/w500${castMember.profilePath}`} alt={castMember.name} className="profilePath" />
+            <p className="name">{castMember.name}</p>
+            <p className="character">{castMember.character}</p>
+          </div>
         ))}
-    </div>
+      </div>
     </div>
   );
 };
--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Header from '../Header';
 import MovieItem from '../MovieItem';
 import './index.css';
@@ -7,15 +7,7 @@ const SearchedMoviePage = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
 
-  useEffect(() => {
-    if (searchQuery.trim() !== '') {
-      fetchSearchResults();
-    } else {
-      setSearchResults([]);
-    }
-  }, [searchQuery]);
-
-  const fetchSearchResults = async () => {
+  const fetchSearchResults = useCallback(async () => {
     try {
       const response = await fetch(
         `https://api.themoviedb.org/3/search/movie?api_key=fa030b3d520335437ba6c4cbb262cfcb&language=en-US&query=${searchQuery}&page=1`
@@ -24,23 +16,26 @@ const SearchedMoviePage = () => {
         throw new Error('Failed to fetch search results');
       }
       const data = await response.json();
-      console.log(data)
-      const {results}=data
-      console.log(results)
-      const updatedData=results.map(eachMovie=>({
-        id:eachMovie.id,
-        title:eachMovie.title,
-        posterPath:eachMovie.poster_path,
-        voteAverage:eachMovie.vote_average,
-      }))
+      const { results } = data;
+      const updatedData = results.map(eachMovie => ({
+        id: eachMovie.id,
+        title: eachMovie.title,
+        posterPath: eachMovie.poster_path,
+        voteAverage: eachMovie.vote_average,
+      }));
       setSearchResults(updatedData);
-      console.log(updatedData)
     } catch (error) {
       console.error('Error fetching search results:', error);
     }
-  };
+  }, [searchQuery]);
 
-  
+  useEffect(() => {
+    if (searchQuery.trim() !== '') {
+      fetchSearchResults();
+    } else {
+      setSearchResults([]);
+    }
+  }, [searchQuery, fetchSearchResults]);
 
   return (
     <div className="searched-movie-container">
@@ -55,8 +50,8 @@ const SearchedMoviePage = () => {
         />
       </div>
       <div className="movies-list">
-        {searchResults.map(eachMovie=>(
-            <MovieItem key={eachMovie.id} itemDetails={eachMovie}/>
+        {searchResults.map(eachMovie => (
+          <MovieItem key={eachMovie.id} itemDetails={eachMovie} />
         ))}
       </div>
     </div>
